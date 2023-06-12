8ec2876e4e5260e86d587d5d4d8453d7b03be923
Pedro Thomé <pedro.th.dev@gmail.com>
2023-06-12 19:24:07 -0300

    Adicionada funcionalidade de listarmagias por school

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var apiUrl = "https://api.open5e.com/spells";
+
 $(document).ready(function() {
 	$('.carousel').slick({
 		arrows: true,
@@ -8,23 +10,70 @@ $(document).ready(function() {
 	});
 
 	$('#search-button').click(function() {
-		var spellName = $('#spell-name').val();
+		let spellName = $('#spell-name').val();
 		if (spellName !== '') {
-			var apiUrl = 'https://api.open5e.com/spells/?name__iexact=' + spellName.replace(/\s+/g, '+');
-			$.getJSON(apiUrl, function(data) {
+			let searchUrl = apiUrl + '/?name__iexact=' + spellName.replace(/\s+/g, '+');
+			$.getJSON(searchUrl, function(data) {
 				if (data.count > 0) {
 					var spell = data.results[0];
-					var spellHtml = '<h2>' + spell.name + '</h2>' +
-									'<p><strong>Tempo de Conjuração:</strong> ' + spell.casting_time + '</p>' +
-									'<p><strong>Alcance:</strong> ' + spell.range + '</p>' +
-									'<p><strong>Componentes:</strong> ' + spell.components + '</p>' +
-									'<p><strong>Duração:</strong> ' + spell.duration + '</p>' +
-									'<p><strong>Nível:</strong> ' + spell.level + '</p>' +
-									'<p><strong>Escola:</strong> ' + spell.school + '</p>' +
-									'<p><strong>Descrição:</strong> ' + spell.desc + '</p>';
+					var spellHtml = `
+						<h2>${spell.name}</h2>
+						<p><strong>Tempo de Conjuração:</strong> ${spell.casting_time}</p>
+						<p><strong>Alcance:</strong> ${spell.range}</p>
+						<p><strong>Componentes:</strong> ${spell.components}</p>
+						<p><strong>Duração:</strong> ${spell.duration}</p>
+						<p><strong>Nível:</strong> ${spell.level}</p>
+						<p><strong>Escola:</strong> ${spell.school}</p>
+						<p><strong>Descrição:</strong> ${spell.desc}</p>
+					`;
 					$('#spell-results').html(spellHtml);
 				} else {
 					$('#spell-results').html('<p>Não foi possível encontrar a magia.</p>');
+				}
+			});
+		}
+	});
+
+	$.getJSON(apiUrl, function(data) {
+		if (data.results) {
+			var schoolsMap = {};
+			data.results.forEach(function(spell) {
+				if (spell.school) {
+					var school = spell.school.toUpperCase(); // Converter para maiúscula
+					schoolsMap[school] = true; // Usar um objeto de mapeamento para evitar duplicatas
+				}
+			});
+			var selectElement = $("#school-select");
+			Object.keys(schoolsMap).forEach(function(school) {
+				selectElement.append($("<option>").val(school).text(school));
+			});
+		}
+	});
+	
+
+	$("#school-select").change(function() {
+		var selectedSchool = $(this).val();
+		if (selectedSchool !== '') {
+			var schoolUrl = apiUrl + '/?school__iexact=' + encodeURIComponent(selectedSchool);
+			$.getJSON(schoolUrl, function(data) {
+				var spellResults = $("#spell-results");
+				spellResults.empty();
+				if (data.results && data.results.length > 0) {
+					data.results.forEach(function(spell) {
+						var spellHtml = `
+							<h2>${spell.name}</h2>
+							<p><strong>Tempo de Conjuração:</strong> ${spell.casting_time}</p>
+							<p><strong>Alcance:</strong> ${spell.range}</p>
+							<p><strong>Componentes:</strong> ${spell.components}</p>
+							<p><strong>Duração:</strong> ${spell.duration}</p>
+							<p><strong>Nível:</strong> ${spell.level}</p>
+							<p><strong>Escola:</strong> ${spell.school}</p>
+							<p><strong>Descrição:</strong> ${spell.desc}</p>
+						`;
+						spellResults.append(spellHtml);
+					});
+				} else {
+					spellResults.append('<p>Não foram encontradas magias para a escola selecionada.</p>');
 				}
 			});
 		}
