9f8775d286eab76c5ccd79fa3233b6eecc20aeeb
Gabriela Ellen <gabrielaellen@MacBook-Air-de-GABRIELA.local>
2023-06-12 23:24:50 -0300

    Mudanto tema do front e carrosel

--- script.js
+++ script.js
@@ -79,3 +79,13 @@ $(document).ready(function() {
 		}
 	});
 });
+
+$('.panel').click(function() {
+	removeActiveClasses();
+	$(this).addClass('active');
+  });
+  
+  function removeActiveClasses() {
+	$('.panel').removeClass('active');
+  }
+  
